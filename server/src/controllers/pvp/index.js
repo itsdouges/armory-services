c4d016e579bf102d8e1b6b38dbd88747f8058382
madou <laheen@gmail.com>
2016-05-15 21:08:18 +1000

    Returns default values for pvp resources

--- server/src/controllers/pvp/index.js
+++ server/src/controllers/pvp/index.js
@@ -1,25 +1,36 @@
 var userHelper = require('../../helpers/get-user-info');
 
+function handleBadToken (defaultValue, error) {
+  if (error === 'Token not found' || error.text === 'requires scope pvp') {
+    return defaultValue;
+  }
+
+  throw error;
+}
+
 function PvpController (models, gw2Api) {
   PvpController.prototype.stats = function (alias) {
     return userHelper.getUserPrimaryToken(models, alias)
       .then(function (token) {
         return gw2Api.readPvpStats(token);
-      });
+      })
+      .catch(handleBadToken.bind(null, {}));
   };
 
   PvpController.prototype.games = function (alias) {
     return userHelper.getUserPrimaryToken(models, alias)
       .then(function (token) {
         return gw2Api.readPvpGames(token);
-      });
+      })
+      .catch(handleBadToken.bind(null, []));
   };
 
   PvpController.prototype.standings = function (alias) {
     return userHelper.getUserPrimaryToken(models, alias)
       .then(function (token) {
         return gw2Api.readPvpStandings(token);
-      });
+      })
+      .catch(handleBadToken.bind(null, []));
   };
 }
 
