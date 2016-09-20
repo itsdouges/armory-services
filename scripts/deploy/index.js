2b046e65d237226b6f8781aed156d0651d0cfc7e
madou <michael.dougall@mi9.com.au>
2016-09-20 12:16:56 +1000

    Adds datadog config to dockerrun, updates deploy to es6

--- scripts/deploy/index.js
+++ scripts/deploy/index.js
@@ -8,31 +8,32 @@ const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
 const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 const IMAGE_UPLOAD_ACCESS_KEY_ID = process.env.IMAGE_UPLOAD_ACCESS_KEY_ID;
 const IMAGE_UPLOAD_SECRET_ACCESS_KEY = process.env.IMAGE_UPLOAD_SECRET_ACCESS_KEY;
+const DATADOG_KEY = process.env.DATADOG_KEY;
 
 if (!ENVIRONMENT) {
-  throw 'Environment variable "ENV" is not defined.';
+  throw new Error('Environment variable "ENV" is not defined.');
 }
 
 if (!ACCESS_KEY_ID) {
-  throw 'Environment variable "ACCESS_KEY_ID" is not defined.';
+  throw new Error('Environment variable "ACCESS_KEY_ID" is not defined.');
 }
 
 if (!SECRET_ACCESS_KEY) {
-  throw 'Environment variable "SECRET_ACCESS_KEY" is not defined.';
+  throw new Error('Environment variable "SECRET_ACCESS_KEY" is not defined.');
 }
 
 if (!IMAGE_UPLOAD_ACCESS_KEY_ID) {
-  throw 'Environment variable "IMAGE_UPLOAD_ACCESS_KEY_ID" is not defined.';
+  throw new Error('Environment variable "IMAGE_UPLOAD_ACCESS_KEY_ID" is not defined.');
 }
 
 if (!IMAGE_UPLOAD_SECRET_ACCESS_KEY) {
-  throw 'Environment variable "IMAGE_UPLOAD_SECRET_ACCESS_KEY" is not defined.';
+  throw new Error('Environment variable "IMAGE_UPLOAD_SECRET_ACCESS_KEY" is not defined.');
 }
 
 const applicationName = 'gw2armory-api';
-const environmentName = 'gw2armory-api-' + ENVIRONMENT.toLowerCase();
+const environmentName = `gw2armory-api-${ENVIRONMENT.toLowerCase()}`;
 
-console.log('Starting deployment for ' + ENVIRONMENT + '!!');
+console.log(`Deploying ${environmentName}...`);
 
 function readModuleFile (path, callback) {
   try {
@@ -43,153 +44,154 @@ function readModuleFile (path, callback) {
   }
 }
 
-console.log('Building dockerrun template');
+console.log('Building dockerrun template...');
 
-readModuleFile('./Dockerrun.aws.json.mustache', function (err, template) {
-  var data = {
-    ENV: ENVIRONMENT,
-    IMAGE_UPLOAD_ACCESS_KEY_ID,
-    IMAGE_UPLOAD_SECRET_ACCESS_KEY,
-    SES_ACCESS_KEY_ID: process.env.SES_ACCESS_KEY_ID,
-    SES_SECRET_ACCESS_KEY: process.env.SES_SECRET_ACCESS_KEY,
-  };
-
-  var output = mustache.render(template, data);
-
-  var ws = fs.createWriteStream('./scripts/deploy/Dockerrun.aws.json');
-  ws.write(output);
-
-  console.log('Done!');
-
-  var datetime = new Date();
-
-  zipConfigs(applicationName, environmentName, datetime);
-});
-
-function zipConfigs (applicationName, environmentName, datetime) {
-  var zipPath = './scripts/deploy/' + applicationName + '-gw2armoryapi:' + new Date().getTime() + '.zip';
-
-  var zip = new EasyZip();
-  zip.addFile('Dockerrun.aws.json', './scripts/deploy/Dockerrun.aws.json', function () {
-    zip.zipFolder('./scripts/deploy/.ebextensions', function () {
-      zip.writeToFile(zipPath);
-      deployToEb(zipPath, environmentName);
-    });
-  });
-}
-
-function deployToEb (zipPath, environmentName) {
-  var application = new EBApplication({
+function deployToEb (zipPath) {
+  const application = new EBApplication({
     accessKeyId: ACCESS_KEY_ID,
     secretAccessKey: SECRET_ACCESS_KEY,
-    region: 'us-east-1'
+    region: 'us-east-1',
   });
 
-  console.log('Pushing to EB');
+  console.log('Pushing to Elastic Beanstalk...');
 
   application.deploy({
     archiveFilePath: zipPath,
-    environmentName: environmentName,
+    environmentName,
+    // eslint-disable-next-line
     awsStackName: '64bit Amazon Linux 2016.03 v2.1.0 running Multi-container Docker 1.9.1 (Generic)',
     // http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
-    beanstalkConfig: [
-    {
+    beanstalkConfig: [{
       Namespace: 'aws:autoscaling:asg',
       OptionName: 'Availability Zones',
-      Value: 'Any 1'
+      Value: 'Any 1',
     },
     {
       Namespace: 'aws:autoscaling:asg',
       OptionName: 'Availability Zones',
-      Value: 'Any 1'
+      Value: 'Any 1',
     },
     {
       Namespace: 'aws:autoscaling:asg',
       OptionName: 'MinSize',
-      Value: '1'
+      Value: '1',
     },
     {
       Namespace: 'aws:autoscaling:asg',
       OptionName: 'MaxSize',
-      Value: '1'
+      Value: '1',
     },
     {
       Namespace: 'aws:autoscaling:launchconfiguration',
       OptionName: 'EC2KeyName',
-      Value: 'gw2armory-ssh'
+      Value: 'gw2armory-ssh',
     },
     {
       Namespace: 'aws:autoscaling:launchconfiguration',
       OptionName: 'IamInstanceProfile',
-      Value: 'aws-elasticbeanstalk-ec2-role'
+      Value: 'aws-elasticbeanstalk-ec2-role',
     },
     {
       Namespace: 'aws:autoscaling:launchconfiguration',
       OptionName: 'InstanceType',
-      Value: 't2.micro'
+      Value: 't2.micro',
     },
     {
       Namespace: 'aws:ec2:vpc',
       OptionName: 'VPCId',
-      Value: 'vpc-7857f31c'
+      Value: 'vpc-7857f31c',
     },
     {
       Namespace: 'aws:ec2:vpc',
       OptionName: 'AssociatePublicIpAddress',
-      Value: 'true'
+      Value: 'true',
     },
     {
       Namespace: 'aws:ec2:vpc',
       OptionName: 'Subnets',
-      Value: 'subnet-75f4db5e,subnet-e235d194,subnet-02e5363f,subnet-851e20dc'
+      Value: 'subnet-75f4db5e,subnet-e235d194,subnet-02e5363f,subnet-851e20dc',
     },
     {
       Namespace: 'aws:elasticbeanstalk:application',
       OptionName: 'Application Healthcheck URL',
-      Value: '/'
+      Value: '/',
     },
     {
-      Namespace : 'aws:elb:listener:443',
-      OptionName : 'SSLCertificateId',
-      Value : 'arn:aws:acm:us-east-1:521573301669:certificate/07c5ada9-db12-4e2d-ba97-69b36d41cf9e'
+      Namespace: 'aws:elb:listener:443',
+      OptionName: 'SSLCertificateId',
+      Value: 'arn:aws:acm:us-east-1:521573301669:certificate/07c5ada9-db12-4e2d-ba97-69b36d41cf9e',
     },
     {
       Namespace: 'aws:elb:listener:443',
       OptionName: 'ListenerProtocol',
-      Value: 'HTTPS'
+      Value: 'HTTPS',
     },
     {
-      Namespace : 'aws:elb:listener:443',
-      OptionName : 'InstancePort',
-      Value : '80'
+      Namespace: 'aws:elb:listener:443',
+      OptionName: 'InstancePort',
+      Value: '80',
     },
     {
-      Namespace : 'aws:elb:listener:443',
-      OptionName : 'InstanceProtocol',
-      Value : 'HTTP'
+      Namespace: 'aws:elb:listener:443',
+      OptionName: 'InstanceProtocol',
+      Value: 'HTTP',
     },
     {
-      Namespace : 'aws:elb:listener:80',
-      OptionName : 'ListenerEnabled',
-      Value : 'true'
+      Namespace: 'aws:elb:listener:80',
+      OptionName: 'ListenerEnabled',
+      Value: 'true',
     },
     {
       Namespace: 'aws:elasticbeanstalk:sns:topics',
       OptionName: 'Notification Protocol',
-      Value: 'email'
+      Value: 'email',
     },
     {
       Namespace: 'aws:elasticbeanstalk:sns:topics',
       OptionName: 'Notification Endpoint',
-      Value: 'laheen@gmail.com'
-    }]
-  }).then(function (data) {
-    console.log(data);
-    console.log('Finished deployment!');
+      Value: 'laheen@gmail.com',
+    }],
+  })
+  .then((data) => {
+    console.log('Finished deployment!', data);
     return application.cleanApplicationVersions(applicationName);
-  }, function (error) {
-    console.error('An error occurred deploying to elastic beanstalk.');
+  }, (error) => {
+    console.error('An error occurred while deploying.');
     console.error(error);
     process.exit(1);
   });
 }
+
+
+function zipConfigs () {
+  const zipPath = `./scripts/deploy/${applicationName}-gw2armoryapi:${new Date().getTime()}.zip`;
+
+  const zip = new EasyZip();
+  zip.addFile('Dockerrun.aws.json', './scripts/deploy/Dockerrun.aws.json', () => {
+    zip.zipFolder('./scripts/deploy/.ebextensions', () => {
+      zip.writeToFile(zipPath);
+      deployToEb(zipPath);
+    });
+  });
+}
+
+readModuleFile('./Dockerrun.aws.json.mustache', (err, template) => {
+  const data = {
+    ENV: ENVIRONMENT,
+    IMAGE_UPLOAD_ACCESS_KEY_ID,
+    IMAGE_UPLOAD_SECRET_ACCESS_KEY,
+    DATADOG_KEY,
+    SES_ACCESS_KEY_ID: process.env.SES_ACCESS_KEY_ID,
+    SES_SECRET_ACCESS_KEY: process.env.SES_SECRET_ACCESS_KEY,
+    DATADOG_TAGS: environmentName,
+  };
+
+  const output = mustache.render(template, data);
+
+  const ws = fs.createWriteStream('./scripts/deploy/Dockerrun.aws.json');
+  ws.write(output);
+
+  console.log('Done!');
+
+  zipConfigs();
+});
