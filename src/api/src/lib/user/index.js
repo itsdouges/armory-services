04f49f5552c49f733d60318e676e761613e8c8e5
madou <michael.dougall@mi9.com.au>
2016-12-05 22:02:57 +1100

    Removes default account name

--- src/api/src/lib/user/index.js
+++ src/api/src/lib/user/index.js
@@ -2,7 +2,7 @@ function parseAccountName (userResult) {
   const token = userResult.gw2_api_tokens &&
     userResult.gw2_api_tokens.filter((tkn) => tkn.primary)[0];
 
-  return token ? token.accountName : 'User';
+  return token && token.accountName;
 }
 
 module.exports = {
