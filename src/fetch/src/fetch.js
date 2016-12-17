acc1f7e4d80971ebc441b32eb148b496627d5e68
madou <michael.dougall@mi9.com.au>
2016-12-17 11:58:52 +1100

    Turns on flow

--- src/fetch/src/fetch.js
+++ src/fetch/src/fetch.js
@@ -1,3 +1,5 @@
+// @flow
+
 import Gitter from 'node-gitter';
 import throat from 'throat';
 import { allSettled } from './lib/promise';
@@ -6,17 +8,17 @@ import config from '../config';
 
 const gitter = new Gitter(config.gitter.apiKey);
 
-function humanifyError (error) {
+function humanifyError (error): string {
   return error.status
     ? (`
 [${error.status} ${error.statusText}] - ${error.data.text}
 [${error.config.method}] ${error.config.url}
 ${error.config.headers.Authorization}
 `)
-    : JSON.stingify(error);
+    : JSON.stringify(error);
 }
 
-async function sendToGitter (roomName, message) {
+async function sendToGitter (roomName: string, message: string) {
   try {
     const room = await gitter.rooms.join(`gw2armory/${roomName}`);
     room.send(`\`\`\`${message}\`\`\``);
@@ -30,7 +32,7 @@ const log = async (message) => await sendToGitter('fetch', message);
 
 const hr = '---------------------------------------------------';
 
-function parseResults (results) {
+function parseResults (results: []) {
   const flattenedResults = results.reduce((acc, result) => acc.concat(result.value), []);
   const errors = flattenedResults.filter(({ state }) => state === 'rejected');
   const successes = flattenedResults.filter(({ state }) => state === 'fulfilled');
@@ -41,7 +43,7 @@ function parseResults (results) {
   };
 }
 
-async function logResults (startTime, { errors = [], successes = [] }) {
+async function logResults (startTime: Date, { errors = [], successes = [] }) {
   const endTime = new Date();
 
   await log(`
@@ -72,12 +74,19 @@ ${hr}
   `);
 }
 
-function fetchFactory (models, fetchers) {
+type Token = {
+  token: string,
+  permissions: Array<string>,
+};
+
+type Fetcher = (models: {}, token: Token) => void;
+
+function fetchFactory (models: {}, fetchers: Array<Fetcher>) {
   if (!fetchers || !fetchers.length) {
     throw new Error('\n=== No fetchers available! ===\n');
   }
 
-  async function fetch (token) {
+  async function fetch (token: Token) {
     return await allSettled(fetchers.map((fetcher) => fetcher(models, token)));
   }
 
