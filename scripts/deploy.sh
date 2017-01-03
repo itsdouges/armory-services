1aeb6c042f456c4c04e16595366046ef4c401a0f
Michael Dougall <Michael.Dougall@mi9.com.au>
2017-01-03 22:57:46 +1100

    Turns on deployment for custom branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   ENV=PROD npm run deploy;
+elif [ "$TRAVIS_BRANCH" == "feature/PvpSeasons" ]; then
+  ENV=TEST npm run deploy;
 else
   echo "BRANCH_NOT_SUPPORTED for deploy"
 fi
