094acb487772c7bb819b5b5cf209c8d7a10cdd77
Michael Dougall <Michael.Dougall@mi9.com.au>
2017-01-03 23:14:28 +1100

    Tweaking deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,15 @@
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+# docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+PROD_BRANCH=master
+TEST_BRANCH=feature/PvpSeasons
+
+if [ "$TRAVIS_BRANCH" == $PROD_BRANCH ]; then
+  echo "Deploying to $PROD_BRANCH"
+  bash ./go.sh push
   ENV=PROD npm run deploy;
-elif [ "$TRAVIS_BRANCH" == "feature/PvpSeasons" ]; then
+elif [ "$TRAVIS_BRANCH" == $TEST_BRANCH ]; then
+  echo "Deploying to $TEST_BRANCH"
+  bash ./go.sh push
   ENV=TEST npm run deploy;
 else
   echo "BRANCH_NOT_SUPPORTED for deploy"
