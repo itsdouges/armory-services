6bc85bd3bbf64c4e2ece186e889d01f48fb7bec0
Michael Dougall <Michael.Dougall@mi9.com.au>
2017-01-13 15:50:17 +1100

    Removes email from user resource

--- src/api/controllers/user/index.js
+++ src/api/controllers/user/index.js
@@ -77,6 +77,7 @@ export default function userControllerFactory (models: Models) {
     'id',
     'passwordHash',
     'token',
+    'email',
   ]);
 
   async function read ({ email, alias, ignorePrivacy, excludeChildren }: ReadPublicOptions = {}) {
