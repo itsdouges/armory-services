2b06f9066192da0d48bbd83c6e7c155fc0193cc9
Michael Dougall <michael.dougall@domain.com.au>
2017-05-18 14:02:03 +1000

    Fixes sitemap route

--- src/api/resources/sitemap/index.js
+++ src/api/resources/sitemap/index.js
@@ -6,10 +6,9 @@ import _ from 'lodash';
 
 export default function SitemapResource (server: Server, controller: any) {
   server.get('/sitemap.xml', async (req, res, next) => {
-    res.setHeader('Content-Type', 'text/xml');
-
     try {
       const sitemapIndex = await controller.index();
+      res.setHeader('Content-Type', 'application/xml');
       res.send(200, sitemapIndex);
     } catch (error) {
       console.error(error);
@@ -20,12 +19,11 @@ export default function SitemapResource (server: Server, controller: any) {
   });
 
   server.get(/^\/sitemap-([a-z]+)-(\d+)\.xml/, async (req, res, next) => {
-    res.setHeader('Content-Type', 'text/xml');
-
-    const [resource, page] = req.params;
-
     try {
+      // eslint-disable-next-line quote-props
+      const { '0': resource, '1': page } = req.params;
       const sitemapIndex = await controller.generate(resource, _.toInteger(page));
+      res.setHeader('Content-Type', 'application/xml');
       res.send(200, sitemapIndex);
     } catch (error) {
       console.error(error);
