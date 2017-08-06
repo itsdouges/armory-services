d793849502271ef07ae94abda311d7b4a5e6e159
Michael Dougall <laheen@gmail.com>
2017-08-06 13:14:34 +1000

    chore(beanstalk): revert to load balanced config

--- scripts/beanstalkConfig.js
+++ scripts/beanstalkConfig.js
@@ -1,17 +1,33 @@
 module.exports = ({ zipPath, environmentName }) => ({
   archiveFilePath: zipPath,
   environmentName,
-  // eslint-disable-next-line max-len
+  // eslint-disable-next-line
   awsStackName: '64bit Amazon Linux 2017.03 v2.7.2 running Multi-container Docker 17.03.1-ce (Generic)',
+  // http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
   beanstalkConfig: [{
     Namespace: 'aws:autoscaling:asg',
     OptionName: 'Availability Zones',
     Value: 'Any 1',
   },
   {
+    Namespace: 'aws:autoscaling:asg',
+    OptionName: 'Availability Zones',
+    Value: 'Any 1',
+  },
+  {
+    Namespace: 'aws:autoscaling:asg',
+    OptionName: 'MinSize',
+    Value: '1',
+  },
+  {
+    Namespace: 'aws:autoscaling:asg',
+    OptionName: 'MaxSize',
+    Value: '1',
+  },
+  {
     Namespace: 'aws:autoscaling:launchconfiguration',
     OptionName: 'EC2KeyName',
-    Value: 'gw2a-api',
+    Value: 'gw2armory-ssh',
   },
   {
     Namespace: 'aws:autoscaling:launchconfiguration',
@@ -25,18 +41,18 @@ module.exports = ({ zipPath, environmentName }) => ({
   },
   {
     Namespace: 'aws:ec2:vpc',
-    OptionName: 'AssociatePublicIpAddress',
-    Value: 'true',
-  },
-  {
-    Namespace: 'aws:ec2:vpc',
     OptionName: 'VPCId',
     Value: 'vpc-7857f31c',
   },
   {
     Namespace: 'aws:ec2:vpc',
+    OptionName: 'AssociatePublicIpAddress',
+    Value: 'true',
+  },
+  {
+    Namespace: 'aws:ec2:vpc',
     OptionName: 'Subnets',
-    Value: 'subnet-75f4db5e',
+    Value: 'subnet-75f4db5e,subnet-e235d194,subnet-02e5363f,subnet-851e20dc',
   },
   {
     Namespace: 'aws:elasticbeanstalk:application',
@@ -51,7 +67,32 @@ module.exports = ({ zipPath, environmentName }) => ({
   {
     Namespace: 'aws:elasticbeanstalk:environment',
     OptionName: 'EnvironmentType',
-    Value: 'SingleInstance',
+    Value: 'LoadBalanced',
+  },
+  {
+    Namespace: 'aws:elb:listener:443',
+    OptionName: 'SSLCertificateId',
+    Value: 'arn:aws:acm:us-east-1:521573301669:certificate/07c5ada9-db12-4e2d-ba97-69b36d41cf9e',
+  },
+  {
+    Namespace: 'aws:elb:listener:443',
+    OptionName: 'ListenerProtocol',
+    Value: 'HTTPS',
+  },
+  {
+    Namespace: 'aws:elb:listener:443',
+    OptionName: 'InstancePort',
+    Value: '80',
+  },
+  {
+    Namespace: 'aws:elb:listener:443',
+    OptionName: 'InstanceProtocol',
+    Value: 'HTTP',
+  },
+  {
+    Namespace: 'aws:elb:listener:80',
+    OptionName: 'ListenerEnabled',
+    Value: 'true',
   },
   {
     Namespace: 'aws:elasticbeanstalk:sns:topics',
