7f9597dd29989adaac5e89133155bbad786efe25
Michael Dougall <laheen@gmail.com>
2017-08-28 10:19:05 +1000

    chore(routes): updates root response

--- src/api/resources/index.js
+++ src/api/resources/index.js
@@ -12,7 +12,9 @@ const down = 'DOWN';
 
 export default function IndexResource (server: Server) {
   server.get('/', (req, res, next) => {
-    res.send(200, 'Hi! Like looking at data? Check out https://github.com/madou/armory-react or https://github.com/madou/armory-back if you want to contribute to Guild Wars 2 Armory!');
+    res.send(200, {
+      message: 'Hi! Like looking at data? Check out https://github.com/madou/armory-react or https://github.com/madou/armory-back if you want to contribute to Guild Wars 2 Armory!',
+    });
     return next();
   });
 
