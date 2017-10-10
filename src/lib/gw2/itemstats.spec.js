955c2a7eff2549fbd7730d7977d51f1db8c1ea55
Hagai Cohen <hagai.co@gmail.com>
2017-10-10 14:27:19 +0300

    fix(itemstats): works for equipments as well

--- src/lib/gw2/itemstats.spec.js
+++ src/lib/gw2/itemstats.spec.js
@@ -1,5 +1,29 @@
 import applyAttributes from './itemstats';
 
+// PoF
+const STAT_MARSHALS = {
+  id: 1378,
+  name: "Marshal's",
+  attributes: {
+    Power: 0.3,
+    Precision: 0.165,
+    Healing: 0.3,
+    ConditionDamage: 0.165
+  }
+};
+
+const STAT_GRIEVING = {
+  id: 1379,
+  name: "Grieving",
+  attributes: {
+    Power: 0.3,
+    Precision: 0.165,
+    CritDamage: 0.165,
+    ConditionDamage: 0.3
+  }
+};
+
+
 // New Stat (Quad Attrib)
 const STAT_MINSTREL = {
   id: 1134,
@@ -61,6 +85,58 @@ const STAT_WIRED = {
   },
 };
 
+const SELECTABLE_ACCESORRY = {
+    name: "Spectral Talisman",
+    description: "<c=@flavor>Resonates with the otherworldly power of Gorseval the Multifarious.</c>",
+    type: "Trinket",
+    level: 80,
+    rarity: "Ascended",
+    vendor_value: 413,
+    game_types: [
+      "Activity",
+      "Wvw",
+      "Dungeon",
+      "Pve"
+    ],
+    flags: [
+      "HideSuffix",
+      "AccountBound",
+      "NoSell",
+      "NotUpgradeable",
+      "DeleteWarning",
+      "AccountBindOnUse"
+    ],
+    restrictions: [],
+    id: 77422,
+    chat_link: "[&AgFuLgEA]",
+    icon: "https://render.guildwars2.com/file/775402D23629AE6F42487B336280E19FDB7E0843/1302752.png",
+    details: {
+      type: "Accessory",
+      infusion_slots: [
+        {
+          flags: [
+            "Infusion"
+          ]
+        }
+      ],
+      secondary_suffix_item_id: "",
+      stat_choices: [
+        1366,
+        1374,
+        1119,
+        584,
+        1128,
+        1163,
+        658,
+        586,
+        1267,
+        1263,
+        1264,
+        1271
+      ]
+    }
+  };
+
 const SELECTABLE_RING = {
   name: 'Black Ice Band',
   description: '<c=@flavor>An icy chill emanates from the ring.</c>',
@@ -103,6 +179,215 @@ const SELECTABLE_RING = {
       1145, 1098, 1220,
     ],
   },
+};
+
+const SELECTABLE_ACENDED_ARMOR_CHEST = {
+    name: "Refined Envoy Breastplate",
+    description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+    type: "Armor",
+    level: 80,
+    rarity: "Ascended",
+    vendor_value: 240,
+    default_skin: 7075,
+    game_types: [
+      "Activity",
+      "Wvw",
+      "Dungeon",
+      "Pve"
+    ],
+    flags: [
+      "HideSuffix",
+      "AccountBound",
+      "NoSell",
+      "DeleteWarning",
+      "AccountBindOnUse"
+    ],
+    restrictions: [],
+    id: 80648,
+    chat_link: "[&AgEIOwEA]",
+    icon: "https://render.guildwars2.com/file/2AA96D63CFBA41DD5FE805D94A073BB22E7EBC57/1634569.png",
+    details: {
+      type: "Coat",
+      weight_class: "Heavy",
+      defense: 381,
+      infusion_slots: [
+        {
+          flags: [
+            "Infusion"
+          ]
+        }
+      ],
+      secondary_suffix_item_id: "",
+      stat_choices: [
+        1377,
+        1379,
+        1378,
+        155,
+        161,
+        159,
+        157,
+        158,
+        160,
+        153,
+        605,
+        700,
+        616,
+        154,
+        156,
+        162,
+        686,
+        559,
+        754,
+        753,
+        799,
+        1026,
+        1067,
+        1226,
+        1225,
+        1229,
+        1224,
+        1228,
+        1227,
+        1231,
+        1232
+      ]
+    }
+  };
+
+const SELECTABLE_LEGEND_ARMOR_CHEST = {
+    name: "Perfected Envoy Breastplate",
+    description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+    type: "Armor",
+    level: 80,
+    rarity: "Legendary",
+    vendor_value: 240,
+    default_skin: 7157,
+    game_types: [
+      "Activity",
+      "Wvw",
+      "Dungeon",
+      "Pve"
+    ],
+    flags: [
+      "HideSuffix",
+      "AccountBound",
+      "NoSalvage",
+      "NoSell",
+      "DeleteWarning",
+      "AccountBindOnUse"
+    ],
+    restrictions: [],
+    id: 80254,
+    chat_link: "[&AgF+OQEA]",
+    icon: "https://render.guildwars2.com/file/DACF9B1ACBE8687B6B31ABC0CF295301120D7A67/1634563.png",
+    details: {
+      type: "Coat",
+      weight_class: "Heavy",
+      defense: 381,
+      infusion_slots: [
+        {
+          flags: [
+            "Infusion"
+          ]
+        }
+      ],
+      secondary_suffix_item_id: "",
+      stat_choices: [
+        155,
+        161,
+        159,
+        157,
+        158,
+        160,
+        153,
+        605,
+        700,
+        616,
+        154,
+        156,
+        162,
+        686,
+        559,
+        754,
+        753,
+        799,
+        1026,
+        1067,
+        1123,
+        1140,
+        1085,
+        1153,
+        1118,
+        1131,
+        1111,
+        1109,
+        1222,
+        1344,
+        1363,
+        1364
+      ]
+    }
+  };
+
+const SELECTABLE_ACENDED_WEP_RIFLE = {
+  name: "Sabetha's Malicious Boomstick",
+  description: "<c=@flavor>Pried from Sabetha's cold, dead hands.</c>",
+  type: "Weapon",
+  level: 80,
+  rarity: "Ascended",
+  vendor_value: 10000,
+  default_skin: 6141,
+  game_types: [
+    "Activity",
+    "Wvw",
+    "Dungeon",
+    "Pve"
+  ],
+  flags: [
+    "HideSuffix",
+    "AccountBound",
+    "NoSell",
+    "Unique",
+    "DeleteWarning",
+    "AccountBindOnUse"
+  ],
+  restrictions: [],
+  id: 77363,
+  chat_link: "[&AgEzLgEA]",
+  icon: "https://render.guildwars2.com/file/773EEC16503A09253E2D99400FD26F0B679ED995/1301720.png",
+  details: {
+    type: "Rifle",
+    damage_type: "Physical",
+    min_power: 1035,
+    max_power: 1265,
+    defense: 0,
+    infusion_slots: [
+      {
+        flags: [
+          "Infusion"
+        ]
+      },
+      {
+        flags: [
+          "Infusion"
+        ]
+      }
+    ],
+    secondary_suffix_item_id: "",
+    stat_choices: [
+      1379,
+      1378,
+      160,
+      154,
+      754,
+      700,
+      153,
+      559,
+      1224,
+      1067,
+      1229
+    ]
+  }
 };
 
 function pack (...args) {
@@ -163,6 +448,24 @@ describe('itemStats', () => {
       ));
     });
 
+    it('should work for armor chest with 3 Stats', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_CHEST, STAT_BERSEKER);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 141),
+        attr('Precision', 101),
+        attr('CritDamage', 101),
+      ));
+    });
+
+    it('should work for acended weapon chest with 3 Stats', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_WEP_RIFLE, STAT_BERSEKER);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 251),
+        attr('Precision', 179),
+        attr('CritDamage', 179),
+      ));
+    });
+
     it('should work for ring with 4 Stats (Legacy)', () => {
       const ret = applyAttributes(SELECTABLE_RING, STAT_BERSEKER_VALKYRE);
       expect(ret).to.be.deep.equal(pack(
@@ -193,6 +496,47 @@ describe('itemStats', () => {
         attr('CritDamage', 57),
         attr('Healing', 57),
         attr('ConditionDamage', 57),
+      ));
+    });
+
+    // PoF
+    it('should work for accessory with greiving stat ', () => {
+      const ret = applyAttributes(SELECTABLE_ACCESORRY, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 92),
+        attr('Precision', 49),
+        attr('CritDamage', 49),
+        attr('ConditionDamage', 92),
+      ));
+    });
+
+    it('should work for accessory with marshal stat ', () => {
+      const ret = applyAttributes(SELECTABLE_ACCESORRY, STAT_MARSHALS);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 92),
+        attr('Precision', 49),
+        attr('Healing', 92),
+        attr('ConditionDamage', 49),
+      ));
+    });
+
+    it('should work for legendary armor chest with marshal stat ', () => {
+      const ret = applyAttributes(SELECTABLE_LEGEND_ARMOR_CHEST, STAT_MARSHALS);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 121),
+        attr('Precision', 67),
+        attr('Healing', 121),
+        attr('ConditionDamage', 67),
+      ));
+    });
+
+    it('should work for acended weapon chest with greiving stat ', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_WEP_RIFLE, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 215),
+        attr('Precision', 118),
+        attr('CritDamage', 118),
+        attr('ConditionDamage', 215),
       ));
     });
   });
