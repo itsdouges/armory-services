9f9c49d8006f0d1633d0b069f19e2efc74679499
Hagai Cohen <hagai.co@gmail.com>
2017-10-10 16:44:43 +0300

    test: refine itemstat testing

--- src/lib/gw2/itemstats.spec.js
+++ src/lib/gw2/itemstats.spec.js
@@ -181,153 +181,519 @@ const SELECTABLE_RING = {
   },
 };
 
-const SELECTABLE_ACENDED_ARMOR_CHEST = {
-    name: "Refined Envoy Breastplate",
-    description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
-    type: "Armor",
-    level: 80,
-    rarity: "Ascended",
-    vendor_value: 240,
-    default_skin: 7075,
-    game_types: [
-      "Activity",
-      "Wvw",
-      "Dungeon",
-      "Pve"
-    ],
-    flags: [
-      "HideSuffix",
-      "AccountBound",
-      "NoSell",
-      "DeleteWarning",
-      "AccountBindOnUse"
-    ],
-    restrictions: [],
-    id: 80648,
-    chat_link: "[&AgEIOwEA]",
-    icon: "https://render.guildwars2.com/file/2AA96D63CFBA41DD5FE805D94A073BB22E7EBC57/1634569.png",
-    details: {
-      type: "Coat",
-      weight_class: "Heavy",
-      defense: 381,
-      infusion_slots: [
-        {
-          flags: [
-            "Infusion"
-          ]
-        }
-      ],
-      secondary_suffix_item_id: "",
-      stat_choices: [
-        1377,
-        1379,
-        1378,
-        155,
-        161,
-        159,
-        157,
-        158,
-        160,
-        153,
-        605,
-        700,
-        616,
-        154,
-        156,
-        162,
-        686,
-        559,
-        754,
-        753,
-        799,
-        1026,
-        1067,
-        1226,
-        1225,
-        1229,
-        1224,
-        1228,
-        1227,
-        1231,
-        1232
-      ]
-    }
-  };
-
 const SELECTABLE_LEGEND_ARMOR_CHEST = {
-    name: "Perfected Envoy Breastplate",
-    description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
-    type: "Armor",
-    level: 80,
-    rarity: "Legendary",
-    vendor_value: 240,
-    default_skin: 7157,
-    game_types: [
-      "Activity",
-      "Wvw",
-      "Dungeon",
-      "Pve"
-    ],
-    flags: [
-      "HideSuffix",
-      "AccountBound",
-      "NoSalvage",
-      "NoSell",
-      "DeleteWarning",
-      "AccountBindOnUse"
-    ],
-    restrictions: [],
-    id: 80254,
-    chat_link: "[&AgF+OQEA]",
-    icon: "https://render.guildwars2.com/file/DACF9B1ACBE8687B6B31ABC0CF295301120D7A67/1634563.png",
-    details: {
-      type: "Coat",
-      weight_class: "Heavy",
-      defense: 381,
-      infusion_slots: [
-        {
-          flags: [
-            "Infusion"
-          ]
-        }
-      ],
-      secondary_suffix_item_id: "",
-      stat_choices: [
-        155,
-        161,
-        159,
-        157,
-        158,
-        160,
-        153,
-        605,
-        700,
-        616,
-        154,
-        156,
-        162,
-        686,
-        559,
-        754,
-        753,
-        799,
-        1026,
-        1067,
-        1123,
-        1140,
-        1085,
-        1153,
-        1118,
-        1131,
-        1111,
-        1109,
-        1222,
-        1344,
-        1363,
-        1364
-      ]
-    }
-  };
+	name: "Perfected Envoy Breastplate",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Legendary",
+	vendor_value: 240,
+	default_skin: 7157,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSalvage",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80254,
+	chat_link: "[&AgF+OQEA]",
+	icon: "https://render.guildwars2.com/file/DACF9B1ACBE8687B6B31ABC0CF295301120D7A67/1634563.png",
+	details: {
+		type: "Coat",
+		weight_class: "Heavy",
+		defense: 381,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1123,
+			1140,
+			1085,
+			1153,
+			1118,
+			1131,
+			1111,
+			1109,
+			1222,
+			1344,
+			1363,
+			1364
+		]
+	}
+};
+
+const SELECTABLE_ACENDED_ARMOR_CHEST = {
+	name: "Refined Envoy Breastplate",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7075,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80648,
+	chat_link: "[&AgEIOwEA]",
+	icon: "https://render.guildwars2.com/file/2AA96D63CFBA41DD5FE805D94A073BB22E7EBC57/1634569.png",
+	details: {
+		type: "Coat",
+		weight_class: "Heavy",
+		defense: 381,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
+
+
+const SELECTABLE_ACENDED_ARMOR_HELM = {
+	name: "Refined Envoy Helmet",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7072,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80387,
+	chat_link: "[&AgEDOgEA]",
+	icon: "https://render.guildwars2.com/file/08BD6169AA1807487D2D025CA92459D3655B9B53/1634571.png",
+	details: {
+		type: "Helm",
+		weight_class: "Heavy",
+		defense: 127,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
+
+const SELECTABLE_ACENDED_ARMOR_SHOULDERS = {
+	name: "Refined Envoy Pauldrons",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7088,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80236,
+	chat_link: "[&AgFsOQEA]",
+	icon: "https://render.guildwars2.com/file/B253EF22450C0323DDE0BBAEDBA045187FBC3953/1634573.png",
+	details: {
+		type: "Shoulders",
+		weight_class: "Heavy",
+		defense: 127,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
+
+const SELECTABLE_ACENDED_ARMOR_GLOVES = {
+	name: "Refined Envoy Gauntlets",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7153,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80673,
+	chat_link: "[&AgEhOwEA]",
+	icon: "https://render.guildwars2.com/file/A0B07A2BE147AA0D9C960BD877029DEDCF59B479/1634570.png",
+	details: {
+		type: "Gloves",
+		weight_class: "Heavy",
+		defense: 191,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
+
+const SELECTABLE_ACENDED_ARMOR_LEGGINGS = {
+	name: "Refined Envoy Tassets",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7110,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80427,
+	chat_link: "[&AgErOgEA]",
+	icon: "https://render.guildwars2.com/file/33BB05D56F5C0F79151F07EFBF4BB0D952346C0E/1634572.png",
+	details: {
+		type: "Leggings",
+		weight_class: "Heavy",
+		defense: 254,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
+
+const SELECTABLE_ACENDED_ARMOR_BOOTS = {
+	name: "Refined Envoy Greaves",
+	description: "<c=@flavor>This armor attempts to channel the power of the envoys.</c>",
+	type: "Armor",
+	level: 80,
+	rarity: "Ascended",
+	vendor_value: 240,
+	default_skin: 7109,
+	game_types: [
+		"Activity",
+		"Wvw",
+		"Dungeon",
+		"Pve"
+	],
+	flags: [
+		"HideSuffix",
+		"AccountBound",
+		"NoSell",
+		"DeleteWarning",
+		"AccountBindOnUse"
+	],
+	restrictions: [],
+	id: 80127,
+	chat_link: "[&AgH/OAEA]",
+	icon: "https://render.guildwars2.com/file/196BE3FD2C2301EFEFE22E98E3FFFC41C8069878/1634568.png",
+	details: {
+		type: "Boots",
+		weight_class: "Heavy",
+		defense: 191,
+		infusion_slots: [
+			{
+				flags: [
+					"Infusion"
+				]
+			}
+		],
+		secondary_suffix_item_id: "",
+		stat_choices: [
+			1377,
+			1379,
+			1378,
+			155,
+			161,
+			159,
+			157,
+			158,
+			160,
+			153,
+			605,
+			700,
+			616,
+			154,
+			156,
+			162,
+			686,
+			559,
+			754,
+			753,
+			799,
+			1026,
+			1067,
+			1226,
+			1225,
+			1229,
+			1224,
+			1228,
+			1227,
+			1231,
+			1232
+		]
+	}
+};
 
 const SELECTABLE_ACENDED_WEP_RIFLE = {
   name: "Sabetha's Malicious Boomstick",
@@ -386,6 +752,61 @@ const SELECTABLE_ACENDED_WEP_RIFLE = {
       1224,
       1067,
       1229
+    ]
+  }
+};
+
+const SELECTABLE_ACENDED_WEP_AXE = {
+  name: "Assaulter's Axe of the Unseen",
+  type: "Weapon",
+  level: 80,
+  rarity: "Ascended",
+  vendor_value: 10000,
+  default_skin: 7091,
+  game_types: [
+    "Activity",
+    "Wvw",
+    "Dungeon",
+    "Pve"
+  ],
+  flags: [
+    "HideSuffix",
+    "AccountBound",
+    "NoSell",
+    "DeleteWarning",
+    "AccountBindOnUse"
+  ],
+  restrictions: [],
+  id: 80678,
+  chat_link: "[&AgEmOwEA]",
+  icon: "https://render.guildwars2.com/file/AB9953FD6F5C7DF04A91594D309768BEB51E6E05/1633846.png",
+  details: {
+    type: "Axe",
+    damage_type: "Physical",
+    min_power: 900,
+    max_power: 1100,
+    defense: 0,
+    infusion_slots: [
+      {
+        flags: [
+          "Infusion"
+        ]
+      }
+    ],
+    secondary_suffix_item_id: "",
+    stat_choices: [
+      1378,
+      1379,
+      157,
+      161,
+      753,
+      799,
+      159,
+      162,
+      1227,
+      1231,
+      1228,
+      1232
     ]
   }
 };
@@ -530,7 +951,77 @@ describe('itemStats', () => {
       ));
     });
 
-    it('should work for acended weapon chest with greiving stat ', () => {
+    it('should work for acended armor helm with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_HELM, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 54),
+        attr('Precision', 30),
+        attr('CritDamage', 30),
+        attr('ConditionDamage', 54),
+      ));
+    });
+
+    it('should work for acended armor shoulders with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_SHOULDERS, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 40),
+        attr('Precision', 22),
+        attr('CritDamage', 22),
+        attr('ConditionDamage', 40),
+      ));
+    });
+
+    it('should work for acended armor chest with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_CHEST, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 121),
+        attr('Precision', 67),
+        attr('CritDamage', 67),
+        attr('ConditionDamage', 121),
+      ));
+    });
+
+    it('should work for acended armor gloves with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_GLOVES, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 40),
+        attr('Precision', 22),
+        attr('CritDamage', 22),
+        attr('ConditionDamage', 40),
+      ));
+    });
+
+    it('should work for acended armor leggings with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_LEGGINGS, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 81),
+        attr('Precision', 44),
+        attr('CritDamage', 44),
+        attr('ConditionDamage', 81),
+      ));
+    });
+
+    it('should work for acended armor boots with Grieving stat', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_ARMOR_BOOTS, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 40),
+        attr('Precision', 22),
+        attr('CritDamage', 22),
+        attr('ConditionDamage', 40),
+      ));
+    });
+
+    it('should work for acended weapon axe with greiving stat ', () => {
+      const ret = applyAttributes(SELECTABLE_ACENDED_WEP_AXE, STAT_GRIEVING);
+      expect(ret).to.be.deep.equal(pack(
+        attr('Power', 108),
+        attr('Precision', 59),
+        attr('CritDamage', 59),
+        attr('ConditionDamage', 108),
+      ));
+    });
+
+    it('should work for acended weapon rifle with greiving stat ', () => {
       const ret = applyAttributes(SELECTABLE_ACENDED_WEP_RIFLE, STAT_GRIEVING);
       expect(ret).to.be.deep.equal(pack(
         attr('Power', 215),
